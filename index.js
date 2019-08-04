ecd5001fb33b06acaea6c76471276460bfa864e7
Tomáš Vyčítal <tom.vycital@gmail.com>
2019-08-04 22:32:29 +0200

    chore: fix the build process

--- index.js
+++ index.js
@@ -1,41 +1,31 @@
 // utils
-import util from 'vis-util';
-//vis.util = util;
+import * as util from 'vis-util';
 
 // data
 import { DataSet, DataView, Queue } from 'vis-data';
 
-// // Network.
-// var { Network, Images, dotparser, gephiParser, allOptions } = require('vis-network');
-// vis.Network = Network;
-// vis.network = { Images, dotparser, gephiParser, allOptions };
-// vis.network.convertDot   = function (input) {return vis.network.dotparser.DOTToGraph(input)};
-// vis.network.convertGephi = function (input,options) {return vis.network.gephiParser.parseGephi(input,options)};
+// Network
+import { Network, network } from 'vis-network';
 
-// // Graph3d
-// var { Graph3d, graph3d, Camera, Filter, Point2d, Point3d, Slider, StepNumber } = require('vis-graph3d');
-// vis.Graph3d = Graph3d;
-// vis.graph3d = { graph3d, Camera, Filter, Point2d, Point3d, Slider, StepNumber };
+// Graph3d
+import { Graph3d, graph3d } from 'vis-graph3d';
 
 // Timeline
 import { Timeline, Graph2d, timeline } from 'vis-timeline';
-// vis.Timeline = Timeline;
-// vis.Graph2d = Graph2d;
-// vis.timeline = timeline;
 
 // misc
 import { DOMutil, keycharm, moment, Hammer } from 'vis-timeline';
-// vis.DOMutil = DOMutil;
-// vis.keycharm = keycharm;
-// vis.moment = moment;
-// vis.Hammer = Hammer;
 
 export default {
-	util, 
+	util,
 
-	DataSet, 
-	DataView, 
+	DataSet,
+	DataView,
 	Queue,
+
+	Network, network,
+
+	Graph3d, graph3d,
 
 	Timeline, Graph2d, timeline,
 
--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,24 @@
 import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
-import nodeBuiltins from 'rollup-plugin-node-builtins';
 import babel from 'rollup-plugin-babel';
-import banner from 'rollup-plugin-banner';
+import minify from 'rollup-plugin-babel-minify';
 import genHeader from './lib/header';
+
+const plugins = [
+	nodeResolve({
+		extensions: ['.ts', '.js', '.json']
+	}),
+	commonjs(),
+	babel({
+		extensions: ['.ts', '.js'],
+		runtimeHelpers: true
+	})
+]
+const minPlugins = [
+	...plugins.slice(),
+	// Note: mangling consumes way too much memory on such a big project.
+	minify({ comments: false, mangle: false })
+]
 
 export default [{
 	input: 'index.js',
@@ -11,13 +26,19 @@ export default [{
 		file: 'dist/vis.js',
 		name: 'vis',
 		exports: 'default',
-		format: 'umd'
+		format: 'umd',
+		sourcemap: true,
+		banner: genHeader(),
 	},
-	plugins: [
-		commonjs(),
-		nodeBuiltins(),
-		nodeResolve(),
-		babel(),
-		banner(genHeader())
-	]
+	plugins: plugins,
+}, {
+	input: 'index.js',
+	output: {
+		file: 'dist/vis.min.js',
+		name: 'vis',
+		exports: 'default',
+		format: 'umd',
+		banner: genHeader(),
+	},
+	plugins: minPlugins,
 }]
