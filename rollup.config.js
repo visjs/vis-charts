766595dcef6255a3d5328d3ef6e646816481165d
Tomáš Vyčítal <tom.vycital@gmail.com>
2019-08-11 16:48:00 +0200

    chore: fix the build process (#6)
    * chore: fix the build process
    
    * chore: update dist

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,59 @@
 import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
-import nodeBuiltins from 'rollup-plugin-node-builtins';
 import babel from 'rollup-plugin-babel';
-import banner from 'rollup-plugin-banner';
+import typescript from 'rollup-plugin-typescript2';
+import css from 'rollup-plugin-css-porter';
+import minify from 'rollup-plugin-babel-minify';
 import genHeader from './lib/header';
 
+const plugins = [
+	nodeResolve({
+		extensions: ['.ts', '.js', '.json']
+	}),
+	typescript({
+		tsconfig: 'tsconfig.code.json',
+		abortOnError: false
+	}),
+	commonjs(),
+	babel({
+		extensions: ['.ts', '.js'],
+		runtimeHelpers: true
+	}),
+	css({
+		minified: false,
+		raw: 'dist/vis.css',
+	}),
+]
+const minPlugins = [
+	...plugins.slice(0, -1),
+	css({
+		minified: 'dist/vis.min.css',
+		raw: false,
+	}),
+	// Note: mangling consumes way too much memory on such a big project.
+	minify({ comments: false, mangle: false })
+]
+
 export default [{
-	input: 'index.js',
+	input: 'src/index.ts',
 	output: {
 		file: 'dist/vis.js',
 		name: 'vis',
 		exports: 'default',
-		format: 'umd'
+		format: 'umd',
+		sourcemap: true,
+		banner: genHeader(),
 	},
-	plugins: [
-		commonjs(),
-		nodeBuiltins(),
-		nodeResolve(),
-		babel(),
-		banner(genHeader())
-	]
+	plugins: plugins,
+}, {
+	input: 'src/index.ts',
+	output: {
+		file: 'dist/vis.min.js',
+		name: 'vis',
+		exports: 'default',
+		format: 'umd',
+		sourcemap: true,
+		banner: genHeader(),
+	},
+	plugins: minPlugins,
 }]
